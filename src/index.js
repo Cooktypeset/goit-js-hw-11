80c9abb877af16a35f8f564e5fe6e712c87eac0c
Cooktypeset <tovpyhalitva@gmail.com>
2023-06-29 21:17:11 +0300

    new upd

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
+KEY = 37979441 - f1d7e4c3b344b48b7a2dc7a50
